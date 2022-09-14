c7c49aca6979ada9cf06f0859b1f59fbf327abff
Stephanie <stephaniiehallberg@gmail.com>
2022-09-14 11:50:43 +0200

    feat(initial): shell of gatsby app

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+module.exports = {
+  siteMetadata: {
+    title: 'React: A JavaScript library for building user interfaces',
+    siteUrl: 'https://reactjs.org',
+    rssFeedTitle: 'React',
+    rssFeedDescription: 'A JavaScript library for building user interfaces',
+  },
+  plugins: [],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>New Site</div>
+    );
+  }
+}
+
+export default Home;

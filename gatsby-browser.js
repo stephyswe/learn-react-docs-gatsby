4375f099cb43f9584204f738843df580c18e9d90
Stephanie <stephaniiehallberg@gmail.com>
2022-09-14 12:29:25 +0200

    reset.css & gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+// Import global styles
+require('normalize.css');
+require('./src/css/reset.css');

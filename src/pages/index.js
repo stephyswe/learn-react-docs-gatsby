1b008b1517748bef4c6665dd955f3d15dbd0a621
Stephanie <stephaniiehallberg@gmail.com>
2022-09-15 09:10:56 +0200

    feat(pages): home page with header - components: ButtonLink & TitleAndMetaTags - icons: logo-white - utils: createCanonicalUrl - content: languages.yml

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,179 @@
 import React, {Component} from 'react';
-import Layout from '../components/Layout';
+
+import Layout from 'components/Layout';
+import Container from 'components/Container';
+import Flex from 'components/Flex';
+import TitleAndMetaTags from 'components/TitleAndMetaTags';
+import ButtonLink from 'components/ButtonLink';
+
+import {colors, media, sharedStyles} from 'theme';
+
+import createCanonicalUrl from 'utils/createCanonicalUrl';
+
+import logoWhiteSvg from 'icons/logo-white.svg';
 
 class Home extends Component {
   render() {
     const {data, location} = this.props;
-    return <Layout location={location}>New Site</Layout>;
+    return (
+      <Layout location={location}>
+        <TitleAndMetaTags
+          title="React &ndash; A JavaScript library for building user interfaces"
+          canonicalUrl={createCanonicalUrl('/')}
+        />
+        <div
+          css={{
+            width: '100%',
+          }}>
+          <header
+            css={{
+              backgroundColor: colors.dark,
+              color: colors.white,
+            }}>
+            <div
+              css={{
+                paddingTop: 45,
+                paddingBottom: 10,
+
+                [media.greaterThan('small')]: {
+                  paddingTop: 60,
+                  paddingBottom: 70,
+                },
+
+                [media.greaterThan('xlarge')]: {
+                  paddingTop: 95,
+                  paddingBottom: 85,
+                  maxWidth: 1500, // Positioning of background logo
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                  position: 'relative',
+                  '::before': {
+                    content: ' ',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    backgroundImage: `url(${logoWhiteSvg})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: '100% 100px',
+                    backgroundSize: '50% auto',
+                    opacity: 0.05,
+                  },
+                },
+              }}>
+              <div
+                css={{
+                  // Content should be above absolutely-positioned hero image
+                  position: 'relative',
+                }}>
+                <Container>
+                  <h1
+                    css={{
+                      color: colors.brand,
+                      textAlign: 'center',
+                      margin: 0,
+                      fontSize: 45,
+                      letterSpacing: '0.01em',
+                      [media.size('xsmall')]: {
+                        fontSize: 30,
+                      },
+                      [media.greaterThan('xlarge')]: {
+                        fontSize: 60,
+                      },
+                    }}>
+                    React
+                  </h1>
+                  <p
+                    css={{
+                      paddingTop: 15,
+                      textAlign: 'center',
+                      fontSize: 24,
+                      letterSpacing: '0.01em',
+                      fontWeight: 200,
+
+                      [media.size('xsmall')]: {
+                        fontSize: 16,
+                        maxWidth: '12em',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                      },
+
+                      [media.greaterThan('xlarge')]: {
+                        paddingTop: 20,
+                        fontSize: 30,
+                      },
+                    }}>
+                    A JavaScript library for building user interfaces
+                  </p>
+                  <Flex
+                    valign="center"
+                    halign="center"
+                    css={{
+                      paddingTop: 40,
+                      flexWrap: 'wrap',
+                      justifyContent: 'center',
+
+                      [media.greaterThan('xlarge')]: {
+                        paddingTop: 65,
+                      },
+                    }}>
+                    <CtaItem>
+                      <ButtonLink
+                        to="/docs/getting-started.html"
+                        type="primary">
+                        Get Started
+                      </ButtonLink>
+                    </CtaItem>
+                    <CtaItem>
+                      <ButtonLink to="/tutorial/tutorial.html" type="secondary">
+                        Take the Tutorial
+                      </ButtonLink>
+                    </CtaItem>
+                  </Flex>
+                </Container>
+              </div>
+            </div>
+          </header>
+        </div>
+      </Layout>
+    );
   }
 }
+
+const CtaItem = ({children, primary = false}) => (
+  <div
+    css={{
+      [media.between('small', 'large')]: {
+        paddingLeft: 20,
+      },
+
+      [media.greaterThan('xlarge')]: {
+        paddingLeft: 40,
+      },
+
+      '&:first-child': {
+        textAlign: 'right',
+        paddingRight: 7,
+        paddingLeft: 7,
+        [media.lessThan('small')]: {
+          marginBottom: 10,
+        },
+      },
+
+      '&:nth-child(2)': {
+        paddingRight: 7,
+        paddingLeft: 7,
+        [media.greaterThan('small')]: {
+          paddingLeft: 15,
+        },
+        [media.lessThan('small')]: {
+          marginBottom: 10,
+        },
+      },
+    }}>
+    {children}
+  </div>
+);
 
 export default Home;

70d5e1af49e13204d66598b226c183dccd71ebbe
Stephanie <stephaniiehallberg@gmail.com>
2022-09-16 13:02:04 +0200

    feat(pages-docs): responsive iframe & hooks files - content: docs / ( hooks files )
    gatsby-config: gatsby-remark-responsive-iframe

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          'gatsby-remark-responsive-iframe',
           {
             resolve: 'gatsby-remark-images',
             options: {
